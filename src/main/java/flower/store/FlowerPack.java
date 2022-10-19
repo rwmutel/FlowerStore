49eccca8c24965d011bd67d3abec0d46001c3f90
Roman <romanmutel@gmail.com>
2022-10-19 16:20:41 +0300

    implemented FlowerBucket

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -3,7 +3,11 @@ package flower.store;
 public class FlowerPack {
     private Flower flower;
     private int quantity;
+    private int price = 0;
     public FlowerPack(Flower flower, int quantity){
-
+        price = flower.getPrice() * quantity;
+    }
+    public int getPrice() {
+        return price;
     }
 }
